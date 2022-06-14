a583e06d1ed86e0132d4fdcdbc701be2cbc6dcf6
rerrlofficial <71943299+rerrlofficial@users.noreply.github.com>
2022-06-15 04:40:22 +0530

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,31 @@
+<?php
+    $to = 'contact@naturio.org';
+    $name = $_POST["name"];
+    $email= $_POST["email"];
+    $subject= $_POST["subject"];
+    $text= $_POST["message"];
+    
+
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'. Name: $name.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Subject: '.$subject.'</td></tr>
+        <tr><td>Text: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'The message has been sent successfully. We will get in touch with you soon';
+    }else{
+        echo 'The message could not be sent. Please try again';
+    }
+
+?>
